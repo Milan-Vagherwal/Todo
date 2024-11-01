d1564d7878bc1ab31d503728f5e04ef30df29a48
Milan-Vagheral <vagherwalmilan@gmail.com>
2024-11-01 16:41:44 +0000

    Orignal code

--- index.js
+++ index.js
@@ -1,55 +1,78 @@
-const express = require("express");
-const { UserModel, TodoModel } = require("./db");
-const jwt = require("jsonwebtoken");
+const express = require ("express");
+const  { UserModel, TodoModel }= require("./db");
+const jwt =  require("jsonwebtoken");
 const mongoose = require("mongoose");
-const JWT_SECRET = "Harkirat123"; // Consider using environment variables
+const JWT_SECRET = "Harkirat123";
 mongoose.connect("mongodb+srv://mickyrathormv:Name%40123@cluster0.5bo2u.mongodb.net/todo-milan");
 
 const app = express();
 app.use(express.json());
 
-app.post("/signup", async function(req, res) {
-    const { email, password, name } = req.body;
+app.post("/signup", async function(req, res){
+    const email = req.body.email;
+    const password = req.body.password;
+    const name = req.body.name;
 
-    await UserModel.create({ email, password, name });
-    res.json({ message: "You are signed in" });
+    await UserModel.create({
+        email: email,
+        password: password,
+        name: name
+    })
+    res.json({
+        message: "You are signed in"
+    })
 });
+app.post("/signin", async function(req, res){
+    const email = req.body.email;
+    const password = req.body.password;
 
-app.post("/signin", async function(req, res) {
-    const { email, password } = req.body;
+    const user = await UserModel.findOne({
 
-    const user = await UserModel.findOne({ email, password });
-    if (user) {
-        const token = jwt.sign({ id: user._id }, JWT_SECRET);
-        res.json({ token });
-    } else {
-        res.status(403).json({ message: "Incorrect Credentials" });
+        email: email,
+        password: password
+    });
+    console.log(user);
+
+    if(user) {
+        const token = jwt.sign({
+            id : user._id
+        },JWT_SECRET);
+        res.json({
+            token : token
+        })
+    }
+    else{
+        res.status(403).json({
+            message: "Incorrect Credentials"
+        })
     }
 });
-
-app.post("/todo", auth, function(req, res) {
+app.post("/todo", auth, function(req, res){
     const userId = req.userId;
-    res.json({ userId });
+
+    res.json({
+        userId: userId
+    })
+});
+app.get("/todos", auth, function(req, res){
+    const userId = req.userId;
+
+    res.json({
+        userId: userId
+    })
 });
 
-app.get("/todos", auth, function(req, res) {
-    const userId = req.userId;
-    res.json({ userId });
-});
-
-function auth(req, res, next) {
+function auth(req, res, next ) {
     const token = req.headers.token;
-    try {
-        const decodeData = jwt.verify(token, JWT_SECRET);
-        if (decodeData) {
-            req.userId = decodeData.id;
-            next();  // Proceed to the next middleware or route
-        }
-    } catch (error) {
-        res.status(403).json({ message: "Invalid credentials" });
+    const decodeData = jwt.verify(token,JWT_SECRET);
+
+    if(decodeData){
+        req.userId = decodeData.userId;
+    }
+    else{
+        res.status(403).json({
+            message: "Incorrect credentials"
+        })
     }
 }
-
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
+app.listen(3000);
